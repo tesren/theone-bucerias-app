ac48ab01fec01f056372344faee570a2361b2636
Erick Perez <erickalejandropm117@gmail.com>
2024-12-09 12:57:37 -0600

    Notificacion precio mas bajo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Unit;
 use App\Models\Message;
+use App\Observers\UnitObserver;
 use App\Observers\MessageObserver;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,5 +32,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Message::observe(MessageObserver::class);
+        Unit::observe(UnitObserver::class);
     }
 }
